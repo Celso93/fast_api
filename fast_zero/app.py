105ff44d4c8cd1917d0236814255a97d193df2c8
Celso TM <celso.takashi07@gmail.com>
2025-05-13 21:31:22 -0300

    introduction web fondations

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,27 @@
+
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.get('/html', response_class=HTMLResponse)
+def read_another_root():
+    return """
+    <html>
+      <head>
+        <title>Hello world!</title>
+      </head>
+      <body>
+        <h1>Hello world</h1>
+      </body>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -7,9 +7,18 @@ from fast_zero.app import app
 
 
 def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
+    client = TestClient(app)  # acessar o cliente
 
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_exercicio_ola_mundo_em_html():
+    client = TestClient(app)
+
+    response = client.get('/html')
+
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1>Hello world</h1>' in response.text
